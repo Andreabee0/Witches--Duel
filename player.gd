a010df46f3cebb263cfda8e0023cfccb2c0ef303
Andrea Bernardy <144374200+Andreabee0@users.noreply.github.com>
2025-02-21 22:48:16 -0700

    Implement Player + movement

---
+++ player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+const SPEED = 800.0
+
+func _physics_process(delta: float) -> void:
+	var direction = getMovementVector()
+	
+	if !direction.is_zero_approx():
+		velocity = getMovementVector() * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
+	
+func getMovementVector() -> Vector2:
+	return Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
