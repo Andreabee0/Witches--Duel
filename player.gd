85d6c52db89db61b7cd4d55089dcac5f8d8856ff
Metalit <48568911+Metalit@users.noreply.github.com>
2025-02-23 13:41:43 -0700

    (a bit) simpler movement

--- player.gd
+++ player.gd
@@ -1,18 +1,17 @@
 extends CharacterBody2D
 
-const SPEED = 800.0
+@export var SPEED := 800.0
 
 @warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
-	var direction = getMovementVector()
-	
+	var direction := getMovementVector()
+
 	if !direction.is_zero_approx():
-		velocity = getMovementVector() * SPEED
+		velocity = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity = Vector2(0, 0)
 
 	move_and_slide()
-	
+
 func getMovementVector() -> Vector2:
 	return Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
