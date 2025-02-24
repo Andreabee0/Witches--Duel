0acdc37ed6b0180a32804f1523e967e910430e56
Metalit <48568911+Metalit@users.noreply.github.com>
2025-02-23 17:08:45 -0700

    Rotate and display cursor sprite

--- src/rotating_cursor.gd
+++ src/rotating_cursor.gd
@@ -4,6 +4,8 @@ extends Node2D
 func _physics_process(_delta: float) -> void:
 	var direction := get_look_vector()
 	position = direction * 100
+	rotation = Vector2(1, 0).angle_to(direction)
+	visible = position.length() > 1
 
 
 func get_look_vector() -> Vector2:
