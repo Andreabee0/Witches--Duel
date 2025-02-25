5928db4115a1517faa25ea5f8a721963c007d996
Metalit <48568911+Metalit@users.noreply.github.com>
2025-02-24 22:18:24 -0700

    Cleanup some menus

--- src/main_menu_manager.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _on_quit_button_down():
-    get_tree().quit()
-
-func _on_play_button_down():
-    get_tree().change_scene_to_file("res://scenes/spell_select_menu.tscn")
-func _on_options_button_down():
-    get_tree().change_scene_to_file("res://scenes/options.tscn")
