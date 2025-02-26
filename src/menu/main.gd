ea61aaf118721869c04792b130f21826c583f6c8
Metalit <48568911+Metalit@users.noreply.github.com>
2025-02-26 09:35:03 -0700

    Tweak buttons

--- src/menu/main.gd
+++ src/menu/main.gd
@@ -1,13 +1,13 @@
 extends Control
 
 
-func _on_quit_button_down():
+func _on_quit_pressed():
 	get_tree().quit()
 
 
-func _on_play_button_down():
+func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/spell_select_menu.tscn")
 
 
-func _on_options_button_down():
+func _on_options_pressed():
 	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
